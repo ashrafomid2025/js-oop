70007f0c6ee0a9aeef137d2e3199df0b138ab262
ashrafomid2025 <omidmashraf@gmail.com>
2025-05-17 13:52:47 +0430

    adding multiple classes

--- public/app.js
+++ public/app.js
@@ -7,27 +7,85 @@ class Product{
     }
 }
 
+class SingleProduct {
+    constructor(pro){
+        this.product = pro;
+    }
+    showSingleProduct(){
+        const divChild = document.createElement("div");
+        divChild.className = "div";
+        divChild.innerHTML = `
+        <img src ="${this.product.imgUrl}" class="img">
+        <h1>${this.product.name}</h1>
+        `
+       return divChild;
+    }
+}
 
-
-const productList = {
-    products: [
+class ProductList {
+    products =  [
         new Product("Bike","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbRyANT0TS-3KAXPlkdbDMDX1WncFqVdSrjrvaXIUE31vusdjsef1N22MZVHBktlyNBDA&usqp=CAU")
         ,
         new Product("Race","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYG3VcZRb1ksgWwgeTtmZvgB1y1PGkX9XBqQaPHF3cLMhv7YTMu9Bn3y7jcvrMlVpFwGY&usqp=CAU")
         ,
-       
-],
+        new Product("Race","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYG3VcZRb1ksgWwgeTtmZvgB1y1PGkX9XBqQaPHF3cLMhv7YTMu9Bn3y7jcvrMlVpFwGY&usqp=CAU")
+        , 
+        new Product("Race","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYG3VcZRb1ksgWwgeTtmZvgB1y1PGkX9XBqQaPHF3cLMhv7YTMu9Bn3y7jcvrMlVpFwGY&usqp=CAU")
+        , 
+        new Product("Race","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYG3VcZRb1ksgWwgeTtmZvgB1y1PGkX9XBqQaPHF3cLMhv7YTMu9Bn3y7jcvrMlVpFwGY&usqp=CAU")
+        , 
+        new Product("Race","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYG3VcZRb1ksgWwgeTtmZvgB1y1PGkX9XBqQaPHF3cLMhv7YTMu9Bn3y7jcvrMlVpFwGY&usqp=CAU")
+        , 
+        new Product("Race","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYG3VcZRb1ksgWwgeTtmZvgB1y1PGkX9XBqQaPHF3cLMhv7YTMu9Bn3y7jcvrMlVpFwGY&usqp=CAU")
+        , 
+    ];
+
+constructor(){}
 render(){
     const div = document.getElementById("app");
     for(let pro of this.products){
-        const divChild = document.createElement("div");
-        divChild.className = "div";
-        divChild.innerHTML = `
-        <img src ="${pro.imgUrl}" class="img">
-        <h1>${pro.name}</h1>
-        `
+        const singlePro = new SingleProduct(pro);
+        const divChild = singlePro.showSingleProduct();
         div.append(divChild);
     }
 }
 }
+
+
+
+
+
+
+
+
+const productList = new ProductList();
 productList.render();
+
+
+
+// class human
+class Human{
+    constructor(name,lastName, age, gender){
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    thinking(){
+        console.log("I am thinking....");
+    }
+    walking(){
+        console.log("I am walking");
+    }
+    eating(){
+        console.log("I am eating something.....");
+    }
+    talking(){
+        console.log("I am talking");
+    }
+}
+
+const Person1 = new Human("Ali","Ahmadi",34,"male");
+Person1.lastName;
+const person2 = new Human("Layla","Sharifi",23,"Female");
