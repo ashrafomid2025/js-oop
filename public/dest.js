0f2810be2fce5d198092f63d13d6fd61bafd2f0f
ashrafomid2025 <omidmashraf@gmail.com>
2025-05-23 13:40:30 +0430

    changes in destination finder

--- public/dest.js
+++ public/dest.js
@@ -14,3 +14,59 @@ else{
 }
 
 },1000);
+
+// select input
+const city = document.getElementById("city");
+const maqsad = document.getElementById("maqsad");
+const sorat = document.getElementById("sorat");
+const btn = document.getElementById("btn");
+const showMessage = document.getElementById("showMessage");
+
+const fasilaha = {
+    kabulKandahar: 501,
+    kabulGhazni: 148,
+    kabulMazar: 426,
+    kabulHerat: 1070,
+    kabulJalalAbad: 149,
+    ghazniMazar: this.kabulGhazni + this.kabulMazar
+}
+
+btn.addEventListener("click",()=>{
+    if(sorat.value !==""){
+        let speed = parseFloat(sorat.value);
+        if((city.value==="kabul" && maqsad.value ==="mazar") || (city.value ==="mazar" && maqsad.value==="kabul")){
+            let time = fasilaha.kabulMazar/speed;
+            showMessage.classList.remove("hidden");
+            showMessage.textContent = `
+            شما از${city.value}
+            به طرف 
+            ${maqsad.value}
+            به سرعت 
+            ${sorat.value}
+            کیلومتر در ساعت 
+            در حرکت استید شما نظر به سنجش در مدت 
+            ${time.toString().substring(0,5)}
+            ساعت خواهید رسید
+            `
+        }
+        else if(
+            (city.value ==="ghazni" && maqsad.value ==="kabul") ||
+            (city.value ==="kabul" && maqsad.value ==="ghazni")
+        ){
+               let time = fasilaha.kabulGhazni/speed;
+            showMessage.classList.remove("hidden");
+            showMessage.textContent = `
+            شما از${city.value}
+            به طرف 
+            ${maqsad.value}
+            به سرعت 
+            ${sorat.value}
+            کیلومتر در ساعت 
+            در حرکت استید شما نظر به سنجش در مدت 
+            ${time.toString().substring(0,5)}
+            ساعت خواهید رسید
+            ` 
+        }
+        
+    }
+})
